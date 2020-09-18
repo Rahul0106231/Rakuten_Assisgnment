4219d36ce13cb238396e293fad0e08068421cb07
RAHUL BALIMIDI <rahul.balimidi@jpmchase.com>
2020-09-18 11:51:10 +0530

    Rakuten Initial Project Commit

---
+++ Rakuten_assisgnment/Rakuten_assisgnment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rakuten_assisgnment
+//
+//  Created by Balimidi, Rahul on 18/09/20.
+//  Copyright © 2020 Balimidi, Rahul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
